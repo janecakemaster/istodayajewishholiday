b2a481992e63983f98cb919b15fa4b113fc51679
janecakemaster <janecakemaster@gmail.com>
2012-09-29 18:35:37 -0400

    more python shit

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-app.py
-istodayajewishholiday.com
+import os
+from flask import Flask
 
-Created by Jane Kim on 2012-03-21.
-"""
+app = Flask(__name__)
 
-import holidays
-
-def main():
-    pass
-
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    main()
-
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
