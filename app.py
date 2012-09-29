f733100e159d46a3020c901e5fa151c8b351507e
janecakemaster <janecakemaster@gmail.com>
2012-09-29 18:26:17 -0400

    blank app thingz

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+app.py
+istodayajewishholiday.com
+
+Created by Jane Kim on 2012-03-21.
+"""
+
+import holidays
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
