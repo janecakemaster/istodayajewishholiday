05ebf220c7a46826dbb3ce7f343eba455977a4fe
janecakemaster <janecakemaster@gmail.com>
2012-09-30 07:51:10 -0400

    OMG IT WORKS
    gotta fix edge cases now

--- static/js/application.js
+++ static/js/application.js
@@ -1,11 +1,35 @@
 $(function(){
 	date = new Date();
 	greg_year = date.getFullYear();
+	greg_month = date.getMonth();
+	greg_day = date.getDay();
+	greg_date = date.getDate();
+	isHoliday = false;
+	time = date.getTime();
 	$.ajax({
 		url: '/',
 		data: {greg_year: date.getFullYear()},
 		success: function(data){
-			console.log(data)
+			d = JSON.parse(data);
+			for(var key in d){
+				if(d.hasOwnProperty(key)){
+					currm = d[key][1];
+					currd = d[key][2];
+					if (currm===greg_month && currd ===greg_date && isHoliday===false) {
+						isHoliday=true;
+						$("#yesno").html("Yes");
+						$("#extras").html(key);
+					};
+					
+					// console.log("obj " + d[key]);
+					// console.log("month " + d[key][1]);
+					// console.log("date " + d[key][2]);
+				}
+			}
+			if(isHoliday===false){
+				$("#yesno").html("No");
+			}
+			console.log(d);
 		}
 	});
 });
