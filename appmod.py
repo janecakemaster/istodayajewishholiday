f54072a71b9efdd8c8730a6a8a47ed6e26789207
janecakemaster <janecakemaster@gmail.com>
2012-10-06 16:06:00 -0400

    note added at footer

--- appmod.py
+++ appmod.py
@@ -1,7 +1,9 @@
+import json
+import ast
 import urllib2
 
 url = 'http://www.hebcal.com/hebcal/?v=1;cfg=json;nh=on;year=now;month=x;ss=on;mf=on;m=72;s=on'
 response = urllib2.urlopen(url).read()
-
-pocket luach
-iphone app
+data = ast.literal_eval(response)
+now = data['date']
+dates = data['items']
